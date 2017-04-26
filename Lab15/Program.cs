a1e053c3890a8a0394645ba4207cb742b8479b6c
Alex Meza <ajex995@gmail.com>
2017-04-26 11:35:27 -0500

    first commit

---
+++ Lab15/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+
+                displayMenu = MainMenu();
+
+            }
+
+
+        }
+
+        public static bool MainMenu()
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1) Show Countries");
+            Console.WriteLine("2) Add a Country");
+            Console.WriteLine("3) Delete a country");
+            Console.WriteLine("4) Ouit");
+
+            
+
+            int userInput = Validate.RangeValidator(1, 3);
+
+            if (userInput == 1)
+            {
+
+                Console.WriteLine(ReadFromFile()); 
+
+                Console.WriteLine("option 1");
+                return true;
+            }
+            else if (userInput == 2)
+            {
+
+                string countries = Console.ReadLine();
+
+                WriteToFile(countries);
+
+                Console.WriteLine("option 2");
+                return true;
+            }
+
+            else if (userInput == 3)
+            {
+                Console.WriteLine("delete a country");
+            }
+
+            else if (userInput == 4)
+            {
+                Console.WriteLine("quit");
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public static void WriteToFile(string input)
+        {
+            StreamWriter sw = new StreamWriter("../../World.txt", true); //settings.txt use relative path, relative to exe.
+            sw.WriteLine(input);
+            sw.Close(); //resource management, close so others can use it
+
+        }
+
+        public static string ReadFromFile()
+        {
+            StreamReader rd = new StreamReader("../../World.txt ");
+
+            string FileContent = rd.ReadToEnd();
+
+            rd.Close();
+            return FileContent;
+
+        }
+
+
+
+    }
+}
---
+++ Lab15/Validate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Validate
+    {
+
+        public static int RangeValidator(int min, int max)
+
+        {
+
+            int input;
+
+            //Console.WriteLine($"Please enter an Integer between {min} and {max}");
+
+            input = GetValidInteger();
+
+            while (input < min || input > max)
+
+            {
+
+                Console.WriteLine("Wrong input! please enter number within range!");
+
+                input = GetValidInteger();
+
+            }
+
+            return input;
+
+
+
+        }
+
+        public static int GetValidInteger()
+        {
+            int input;
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("This is not a integer, please enter a integer");
+            }
+            return input;
+        }
+
+
+
+    }
+}
